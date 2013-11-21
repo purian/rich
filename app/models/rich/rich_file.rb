68c10efcc7e99faf34f579a3e6205ef371d52867
purian <purian@gmail.com>
2013-11-21 09:59:59 +0200

    Change the file limit size

--- app/models/rich/rich_file.rb
+++ app/models/rich/rich_file.rb
@@ -18,7 +18,7 @@ module Rich
     
     validates_attachment_presence :rich_file
     validate :check_content_type
-    validates_attachment_size :rich_file, :less_than=>15.megabyte, :message => "must be smaller than 15MB"
+    validates_attachment_size :rich_file, :less_than=>500.megabyte, :message => "must be smaller than 500MB"
     
     before_create :clean_file_name
 
